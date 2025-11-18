37816ca606c0269dbba4411eb8d090f16d84a096
y2q-actionman <y2q-actionman@users.noreply.github.com>
2025-11-18 13:53:30 +0900

    cleaned demo.lisp

--- demo.lisp
+++ demo.lisp
@@ -2,22 +2,21 @@
 
 (ql:quickload "hunchentoot-recycle")
 
-;;; Start
-
-(defparameter *test-server*
-  (let* ((taskmaster
-      	   (make-instance 'hunchentoot-recycle:recycling-taskmaster
-                          :initial-thread-count 8))
-         (acceptor (make-instance 'hunchentoot-recycle:parallel-easy-acceptor
-                                  :port 4242
-                                  :taskmaster taskmaster)))
-    (hunchentoot:start acceptor)))
-
-;;; Show a test page, from the doc of Hunchentoot
+;;; Defines a test page, from the doc of Hunchentoot
 
 (hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
   (setf (hunchentoot:content-type*) "text/plain")
   (format nil "Hey~@[ ~A~]!" name))
+
+;;; Start
+
+(defparameter *test-server*
+  (let* ((taskmaster
+      	   (make-instance 'hunchentoot-recycle:recycling-taskmaster))
+         (acceptor (make-instance 'hunchentoot-recycle:parallel-easy-acceptor
+                                  :port 4242
+                                  :taskmaster taskmaster)))
+    (hunchentoot:start acceptor)))
 
 ; See "http://127.0.0.1:4242/yo" or "http://127.0.0.1:4242/yo?name=Dude" .
 
