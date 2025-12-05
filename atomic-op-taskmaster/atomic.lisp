990a2f92ccdb365adbef943b218d517532c3fd2d
y2q-actionman <y2q-actionman@users.noreply.github.com>
2025-12-05 16:09:09 +0900

    Cleanup atomic-op-taskmaster

--- atomic-op-taskmaster/atomic.lisp
+++ atomic-op-taskmaster/atomic.lisp
@@ -3,87 +3,97 @@
 (defclass atomic-taskmaster (hunchentoot:one-thread-per-connection-taskmaster)
   ((hunchentoot::thread-count
     :type bt2:atomic-integer
-    :initform (bt2:make-atomic-integer)
-    :accessor atomic-taskmaster-thread-count-cell
-    :documentation
-    "Works same as the original one except it is an atomic-integer.")
+    :initform (bt2:make-atomic-integer))
    (hunchentoot::accept-count
     :type bt2:atomic-integer
-    :initform (bt2:make-atomic-integer)
-    :accessor atomic-taskmaster-accept-count-cell
-    :documentation
-    "Works same as the original one except it is an atomic-integer."))
+    :initform (bt2:make-atomic-integer)))
   (:documentation "A taskmaster works just like
 `hunchentoot:one-thread-per-connection-taskmaster' except using
 atomic integers."))
 
-(defmethod hunchentoot::taskmaster-accept-count ((taskmaster atomic-taskmaster))
-  (bt2:atomic-integer-value (atomic-taskmaster-accept-count-cell taskmaster)))
+(in-package #:hunchentoot) ; because I use internal symbols of Hunchentoot.
 
-(defmethod hunchentoot::increment-taskmaster-accept-count ((taskmaster atomic-taskmaster))
-  (when (hunchentoot::taskmaster-max-accept-count taskmaster)
-    (bt2:atomic-integer-incf (atomic-taskmaster-accept-count-cell taskmaster))))
+(defmethod taskmaster-accept-count ((taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (bt2:atomic-integer-value (slot-value taskmaster 'accept-count)))
 
-(defmethod hunchentoot::decrement-taskmaster-accept-count ((taskmaster atomic-taskmaster))
-  (when (hunchentoot::taskmaster-max-accept-count taskmaster)
-    (bt2:atomic-integer-decf (atomic-taskmaster-accept-count-cell taskmaster))))
+(defmethod (setf taskmaster-accept-count) (value (taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (setf (bt2:atomic-integer-value (slot-value taskmaster 'accept-count)) value))
 
-(defmethod hunchentoot::taskmaster-thread-count ((taskmaster atomic-taskmaster))
-  (bt2:atomic-integer-value (atomic-taskmaster-thread-count-cell taskmaster)))
+(defmethod increment-taskmaster-accept-count ((taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (when (taskmaster-max-accept-count taskmaster)
+    (bt2:atomic-integer-incf (slot-value taskmaster 'accept-count))))
 
-(defmethod hunchentoot::increment-taskmaster-thread-count ((taskmaster atomic-taskmaster))
-  (when (hunchentoot::taskmaster-max-thread-count taskmaster)
-    (bt2:atomic-integer-incf (atomic-taskmaster-thread-count-cell taskmaster))))
+(defmethod decrement-taskmaster-accept-count ((taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (when (taskmaster-max-accept-count taskmaster)
+    (bt2:atomic-integer-decf (slot-value taskmaster 'accept-count))))
 
-(defmethod hunchentoot::decrement-taskmaster-thread-count ((taskmaster atomic-taskmaster))
-  (when (hunchentoot::taskmaster-max-thread-count taskmaster)
+(defmethod taskmaster-thread-count ((taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (bt2:atomic-integer-value (slot-value taskmaster 'thred-count)))
+
+(defmethod (setf taskmaster-thread-count) (value (taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (setf (bt2:atomic-integer-value (slot-value taskmaster 'thred-count)) value))
+
+(defmethod taskmaster-thread-count ((taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (bt2:atomic-integer-value (slot-value taskmaster 'thread-count)))
+
+(defmethod increment-taskmaster-thread-count ((taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (when (taskmaster-max-thread-count taskmaster)
+    (bt2:atomic-integer-incf (slot-value taskmaster 'thread-count))))
+
+(defmethod decrement-taskmaster-thread-count ((taskmaster hunchentoot-atomic-op-taskmaster:atomic-taskmaster))
+  (when (taskmaster-max-thread-count taskmaster)
     (prog2
-        (bt2:atomic-integer-decf (atomic-taskmaster-thread-count-cell taskmaster))
-        (hunchentoot::decrement-taskmaster-accept-count taskmaster)
-      (when (and (hunchentoot::taskmaster-max-accept-count taskmaster)
-                 (< (hunchentoot::taskmaster-thread-count taskmaster)
-                    (hunchentoot::taskmaster-max-accept-count taskmaster)))
-        (hunchentoot::note-free-connection taskmaster)))))
+        (bt2:atomic-integer-decf (slot-value taskmaster 'thread-count))
+        (decrement-taskmaster-accept-count taskmaster)
+      (when (and (taskmaster-max-accept-count taskmaster)
+                 (< (taskmaster-thread-count taskmaster)
+                    (taskmaster-max-accept-count taskmaster)))
+        (note-free-connection taskmaster)))))
 
+
+(in-package #:hunchentoot-atomic-op-taskmaster)
 
 (defclass atomic-acceptor (hunchentoot:acceptor)
   ((hunchentoot::requests-in-progress
     :type bt2:atomic-integer
-    :initform (bt2:make-atomic-integer)
-    :accessor atomic-acceptor-requests-in-progress-cell
-    :documentation
-    "Works same as the original one except it is an atomic-integer."))
+    :initform (bt2:make-atomic-integer)))
   (:documentation "An acceptor works just like
 `hunchentoot:acceptor' except using atomic integers."))
 
-(defmethod hunchentoot::acceptor-requests-in-progress ((acceptor atomic-acceptor))
-  (bt2:atomic-integer-value (atomic-acceptor-requests-in-progress-cell acceptor)))
-
-(defgeneric hunchentoot::do-with-acceptor-request-count-incremented (acceptor function))
-
 (in-package #:hunchentoot)
 
-;;; same as the original 
-(defmethod do-with-acceptor-request-count-incremented (*acceptor* function)
-  (with-lock-held ((acceptor-shutdown-lock *acceptor*))
-    (incf (acceptor-requests-in-progress *acceptor*)))
-  (unwind-protect
-       (funcall function)
+(defmethod acceptor-requests-in-progress ((acceptor hunchentoot-atomic-op-taskmaster:atomic-acceptor))
+  (bt2:atomic-integer-value (slot-value acceptor 'requests-in-progress)))
+
+(defmethod (setf acceptor-requests-in-progress) (value (acceptor hunchentoot-atomic-op-taskmaster:atomic-acceptor))
+  (setf (bt2:atomic-integer-value (slot-value acceptor 'requests-in-progress)) value))
+
+(defgeneric hunchentoot::do-with-acceptor-request-count-incremented (acceptor function)
+  (:documentation "This definition overwrites the original one with a
+  generic function to hook accessing atomic integers.")
+  (:method (*acceptor* function)
+    "Default method is same as the original one"
     (with-lock-held ((acceptor-shutdown-lock *acceptor*))
-      (decf (acceptor-requests-in-progress *acceptor*))
-      (when (acceptor-shutdown-p *acceptor*)
-        (condition-variable-signal (acceptor-shutdown-queue *acceptor*))))))
+      (incf (acceptor-requests-in-progress *acceptor*)))
+    (unwind-protect
+         (funcall function)
+      (with-lock-held ((acceptor-shutdown-lock *acceptor*))
+        (decf (acceptor-requests-in-progress *acceptor*))
+        (when (acceptor-shutdown-p *acceptor*)
+          (condition-variable-signal (acceptor-shutdown-queue *acceptor*))))))
+  (:method ((*acceptor* hunchentoot-atomic-op-taskmaster:atomic-acceptor) function)
+    "Do same as the original one except utilizing atomic integers."
+    (with-slots (requests-in-progress) *acceptor*
+      (bt2:atomic-integer-incf requests-in-progress)
+      (unwind-protect
+           (funcall function)
+        (bt2:atomic-integer-decf requests-in-progress)
+        (with-lock-held ((acceptor-shutdown-lock *acceptor*))
+          (when (acceptor-shutdown-p *acceptor*)
+            (condition-variable-signal (acceptor-shutdown-queue *acceptor*))))))))
+
 
 (in-package #:hunchentoot-atomic-op-taskmaster)
-
-(defmethod hunchentoot::do-with-acceptor-request-count-incremented ((hunchentoot::*acceptor* atomic-acceptor) function)
-  (bt2:atomic-integer-incf (atomic-acceptor-requests-in-progress-cell hunchentoot::*acceptor*))
-  (unwind-protect
-       (funcall function)
-    (bt2:atomic-integer-decf (atomic-acceptor-requests-in-progress-cell hunchentoot::*acceptor*))
-    (hunchentoot::with-lock-held ((hunchentoot::acceptor-shutdown-lock hunchentoot::*acceptor*))
-      (when (hunchentoot::acceptor-shutdown-p hunchentoot::*acceptor*)
-        (hunchentoot::condition-variable-signal (hunchentoot::acceptor-shutdown-queue hunchentoot::*acceptor*))))))
 
 ;;; Derived classes
 (defclass atomic-ssl-acceptor (atomic-acceptor hunchentoot:ssl-acceptor)
