29dfb79ca894166758c2a67b8c9fefd467d55aa1
y2q-actionman <y2q-actionman@users.noreply.github.com>
2025-12-18 16:24:09 +0900

    Added shutdown-in-hello-world-handler test

--- test/recycling-taskmaster-test/shutdown.lisp
+++ test/recycling-taskmaster-test/shutdown.lisp
@@ -1,38 +1,64 @@
 (in-package #:hunchentoot-recycling-taskmaster-test)
 
+;;; Define functions to stop worker threads at some point.
+
+(defconstant +bitly-after-sleep+ 0.1)
+
 (defmacro define-debug-stopper (name-prefix)
   "Defines functions to stop threads for testing `hunchentoot:shutdown'."
   (let ((boolean-storage-name (intern (format nil "*~A-~A*" name-prefix '#:stop-boolean)))
         (boolean-accessor-name (intern (format nil "~A-~A" name-prefix '#:stop-p)))
         (lock-name (intern (format nil "*~A-~A*" name-prefix '#:lock)))
         (condition-variable-name (intern (format nil "*~A-~A*" name-prefix '#:condition-variable)))
-        (wait-function-name (intern (format nil "~A-~A" name-prefix '#:wait)))
-        (notify-function-name (intern (format nil "~A-~A" name-prefix '#:notify))))
+        (waiter-counter-name (intern (format nil "*~A-~A*" name-prefix '#:waiter-counter)))
+        (stop-function-name (intern (format nil "~A-~A" '#:stop-at name-prefix)))
+        (sleep-for-a-worker-in-function-name
+          (intern (format nil "~A-~A" '#:sleep-for-a-worker-in name-prefix)))
+        (schedule-waking-up-a-worker-function-name
+          (intern (format nil "~A-~A" '#:schedule-waking-up-a-worker-in name-prefix))))
     `(progn
        (defparameter ,boolean-storage-name
          nil)
-       (defun ,boolean-accessor-name ()
-         ,boolean-storage-name)
-       (defun (setf ,boolean-accessor-name) (value)
-         (setf ,boolean-storage-name value))
        (defvar ,lock-name
          (make-lock ,(string lock-name)))
        (defvar ,condition-variable-name
          (make-condition-variable :name ,(string condition-variable-name)))
-       (defun ,wait-function-name ()
+       (defvar ,waiter-counter-name
+         0)
+       (defun ,boolean-accessor-name ()
+         (with-lock-held (,lock-name)
+           ,boolean-storage-name))
+       (defun (setf ,boolean-accessor-name) (value)
+         (with-lock-held (,lock-name)
+           (setf ,boolean-storage-name value)
+           (condition-notify ,condition-variable-name)))
+       (defun ,stop-function-name ()
+         (with-lock-held (,lock-name)
+           (incf ,waiter-counter-name)
+           (condition-notify ,condition-variable-name)
+           (unwind-protect
+                (loop
+                  while ,boolean-storage-name
+                  do (condition-wait ,condition-variable-name ,lock-name))
+             (decf ,waiter-counter-name)
+             (condition-notify ,condition-variable-name))))
+       (defun ,sleep-for-a-worker-in-function-name ()
          (with-lock-held (,lock-name)
            (loop
-             while (,boolean-accessor-name)
+             until (plusp ,waiter-counter-name)
              do (condition-wait ,condition-variable-name ,lock-name))))
-       (defun ,notify-function-name ()
-         (with-lock-held (,lock-name)
-           (condition-notify ,condition-variable-name))))))
+       (defun ,schedule-waking-up-a-worker-function-name ()
+         (make-thread (lambda ()
+                        (sleep +bitly-after-sleep+)
+                        (setf (,boolean-accessor-name) nil)))))))
 
 (define-debug-stopper before-handle-incoming-connection)
 (define-debug-stopper after-handle-incoming-connection)
 (define-debug-stopper before-process-connection)
 (define-debug-stopper after-process-connection)
+(define-debug-stopper hello-world-handler)
 
+;;; Server class definitions for debugging.
 
 (defparameter *debug-server-initial-thread* 2)
 (defparameter *debug-server-port* 62532)
@@ -43,10 +69,10 @@
    :initial-thread-count *debug-server-initial-thread*))
 
 (defmethod hunchentoot:handle-incoming-connection :before ((taskmaster recycling-taskmaster-debug) client-connection)
-  (before-handle-incoming-connection-wait))
+  (stop-at-before-handle-incoming-connection))
 
 (defmethod hunchentoot:handle-incoming-connection :after ((taskmaster recycling-taskmaster-debug) client-connection)
-  (after-handle-incoming-connection-wait))
+  (stop-at-after-handle-incoming-connection))
 
 (defclass parallel-acceptor-debug (hunchentoot-recycling-taskmaster:parallel-acceptor)
   ()
@@ -54,15 +80,16 @@
    :taskmaster (make-instance 'recycling-taskmaster-debug)))
 
 (defmethod hunchentoot:process-connection :before ((acceptor parallel-acceptor-debug) socket)
-  (before-process-connection-wait))
+  (stop-at-before-process-connection))
 
 (defmethod hunchentoot:process-connection :after ((acceptor parallel-acceptor-debug) socket)
-  (after-process-connection-wait))
+  (stop-at-after-process-connection))
 
 (defclass parallel-easy-acceptor-debug (parallel-acceptor-debug hunchentoot:easy-acceptor)
   ())
 
 (hunchentoot:define-easy-handler (hello-world :uri "/") ()
+  (stop-at-hello-world-handler)
   (setf (hunchentoot:content-type*) "text/plain")
   "Hello, World!")
 
@@ -80,6 +107,30 @@
        (when ,server
          (hunchentoot:stop ,server)))))
 
-(1am:test no-accept-shutdown
+(defparameter *test-server-url*
+  (format nil "http://127.0.0.1:~D/" *debug-server-port*))
+
+(defun test-request (&key keep-alive)
+  (handler-case
+      (drakma:http-request *test-server-url* :keep-alive keep-alive)
+    (usocket:connection-reset-error ()
+      (progn))))
+
+;;; Test scenarios
+
+(1am:test shutdown-with-no-accept
   (with-making-test-server (server)
     (progn)))
+
+(1am:test shutdown-after-one-accept
+  (with-making-test-server (server)
+    (test-request)))
+
+(1am:test shutdown-in-hello-world-handler
+  (with-making-test-server (server)
+    (setf (hello-world-handler-stop-p) t)
+    (make-thread 'test-request)
+    (sleep-for-a-worker-in-hello-world-handler)
+    ;; Schedule waking up a worker thread bitly after `hunchentoot:stop'.
+    (schedule-waking-up-a-worker-in-hello-world-handler)
+    (hunchentoot:stop (shiftf server nil))))
