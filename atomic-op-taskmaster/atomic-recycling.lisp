5eb9ded68ad87a9e5d0b54568a544f69a741a89b
y2q-actionman <y2q-actionman@users.noreply.github.com>
2025-12-18 10:23:44 +0900

    Small cleanup of atomic-recycling-taskmaster

--- atomic-op-taskmaster/atomic-recycling.lisp
+++ atomic-op-taskmaster/atomic-recycling.lisp
@@ -24,15 +24,17 @@
     :reader recycling-taskmaster-thread-count
     :documentation "The number of how many threads running. This is same with the number of keys in acceptor-process slot, but splited to avoid locking.")))
 
-(defmethod add-recycling-taskmaster-thread :after ((taskmaster atomic-thread-count-recycling-taskmaster) thread)
-  (bt2:atomic-integer-incf (recycling-taskmaster-thread-count taskmaster)))
+(defmethod add-recycling-taskmaster-thread ((taskmaster atomic-thread-count-recycling-taskmaster) thread)
+  (prog1 (call-next-method)
+    (bt2:atomic-integer-incf (recycling-taskmaster-thread-count taskmaster))))
 
 (defmethod remove-recycling-taskmaster-thread ((taskmaster atomic-thread-count-recycling-taskmaster) thread)
   (when-let ((deleted? (call-next-method)))
     (bt2:atomic-integer-decf (recycling-taskmaster-thread-count taskmaster))
     deleted?))
 
-(defmethod count-recycling-taskmaster-thread ((taskmaster atomic-thread-count-recycling-taskmaster) &key)
+(defmethod count-recycling-taskmaster-thread ((taskmaster atomic-thread-count-recycling-taskmaster) &key lock)
+  (declare (ignore lock))
   (bt2:atomic-integer-value (recycling-taskmaster-thread-count taskmaster)))
 
 (defmethod delete-recycling-taskmaster-finished-thread ((taskmaster atomic-thread-count-recycling-taskmaster))
@@ -40,4 +42,3 @@
     (bt2:atomic-integer-decf (recycling-taskmaster-thread-count taskmaster)
                              deleted-cnt)
     deleted-cnt))
-
