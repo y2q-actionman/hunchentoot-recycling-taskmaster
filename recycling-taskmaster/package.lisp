394c057a1679583447c5a2b3ece1d9e27c178bcd
y2q-actionman <y2q-actionman@users.noreply.github.com>
2025-12-27 00:06:42 +0900

    Cleaned package.lisp

--- recycling-taskmaster/package.lisp
+++ recycling-taskmaster/package.lisp
@@ -12,9 +12,4 @@
    ;; Only for handling a broken server.
    #:abandon-acceptor
    #:recycling-taskmaster-corrupted-error
-   ;; exported for hunchentoot-atomic-op-taskmaster
-   #:recycling-taskmaster-busy-thread-count
-   #:recycling-taskmaster-busy-thread-count-cell
-   #:recycling-taskmaster-busy-thread-count-lock
-   #:recycling-taskmaster-busy-thread-count-queue
-   #:recycling-taskmaster-acceptor-process-lock))
+   ))
