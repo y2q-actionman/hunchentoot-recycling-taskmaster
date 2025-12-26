8a3d83703b7eca51a143778925be73426fbc7422
y2q-actionman <y2q-actionman@users.noreply.github.com>
2025-12-27 00:07:22 +0900

    Added partial README update

--- demo.lisp
+++ demo.lisp
@@ -3,21 +3,20 @@
 (ql:quickload "hunchentoot-recycling-taskmaster")
 
 ;;; Defines a test page, from the doc of Hunchentoot
-
 (hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
   (setf (hunchentoot:content-type*) "text/plain")
   (format nil "Hey~@[ ~A~]!" name))
 
 ;;; Start
-
 (defparameter *test-server*
-  (let* ((acceptor (make-instance 'hunchentoot-recycling-taskmaster:parallel-easy-acceptor
-                                  :port 4242)))
-    (hunchentoot:start acceptor)))
+  (make-instance 'hunchentoot-recycling-taskmaster:parallel-easy-acceptor
+                 :port 4242))
+(hunchentoot:start *test-server*)
 
-; See "http://127.0.0.1:4242/yo" or "http://127.0.0.1:4242/yo?name=Dude" .
-
+;;; See "http://127.0.0.1:4242/yo" or "http://127.0.0.1:4242/yo?name=Dude".
+;;; 
+;;; $ curl "http://127.0.0.1:4242/yo"
+;;; Hey!
 
 ;;; Stop
-
 (hunchentoot:stop *test-server*)
